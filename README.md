361e368a6956fe07a02dabcf52d8a8a13a0b4648
Evyatar <evyatar.a@fiverr.com>
2017-03-02 19:47:05 +0200

    Handle lint issues

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Isomorphic data model validations made easy.
 
-[![npm version](https://badge.fury.io/js/passable.svg)](https://badge.fury.io/js/passable) [![Build Status](https://travis-ci.org/ealush/passable.svg?branch=master)](https://travis-ci.org/ealush/passable)
+[![npm version](https://badge.fury.io/js/passable.svg)](https://badge.fury.io/js/passable) [![Build Status](https://travis-ci.org/ealush/passable.svg?branch=master)](https://travis-ci.org/ealush/passable) [![bitHound Overall Score](https://www.bithound.io/github/ealush/passable/badges/score.svg)](https://www.bithound.io/github/ealush/passable)
 
 ## Installation
 Just `npm install passable --save` and you're all set.
--- spec/passable.api.custom.spec.js
+++ spec/passable.api.custom.spec.js
@@ -17,33 +17,30 @@ describe('Test test extensions', () => {
 
 const noSnuffles = Passable('ExtendTests', (pass, enforce) => {
 
-        pass('NoSnuffles', 'should pass', () => {
-            return enforce('The name is Rick', {
+        pass('NoSnuffles', 'should pass', () => enforce('The name is Rick', {
                 has_no_snuffles: {
                     expect: true
                 }
-            });
-        });
+            })
+        );
 
-        pass('NoSnuffles', 'should Fail', () => {
-            return enforce('The name is snuffles', {
+        pass('NoSnuffles', 'should Fail', () => enforce('The name is snuffles', {
                 has_no_snuffles: {
                     expect: true
                 },
                 largerThan: {
                     testAgainst: 0
                 }
-            });
-        });
+            })
+        );
 
-        pass('regularTest', 'should pass', () => {
-            return enforce('The name is snuffles', {
+        pass('regularTest', 'should pass', () => enforce('The name is snuffles', {
                 largerThan: {
                     expect: false,
                     testAgainst: 42
                 }
-            });
-        });
+            })
+        )
     }, {
         has_no_snuffles: (v) => v.indexOf('snuffles') === -1
     });
