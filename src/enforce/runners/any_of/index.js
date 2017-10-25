2686b69eb91a53da68e6ea214c15801453d1845a
Ronen E <alter.ronen@gmail.com>
2017-10-25 22:39:52 +0300

    Refactor runners

--- src/enforce/runners/any_of/index.js
+++ src/enforce/runners/any_of/index.js
@@ -3,22 +3,6 @@ import run from '../run';
 
 export default function anyOf(value: mixed, tests: Tests, rules: Rules): boolean {
 
-    const testsCount: number = Object.keys(tests).length;
-    let successCount: number = 0;
-
-    if (testsCount === 0) {
-        return false;
-    }
-
-    for (const key: string in tests) {
-
-        const success: boolean = run(value, key, tests, rules);
-
-        if (success === true) {
-            successCount++;
-            break;
-        }
-    }
-
-    return successCount > 0;
+    const validations: Array<string> = Object.keys(tests);
+    return validations.some((key) => run(value, key, tests, rules) === true);
 }
