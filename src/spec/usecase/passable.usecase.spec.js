5986927fc0c6c68a1a07ba4266404891206d2180
Evyatar Alush <evyatar.a@fiverr.com>
2017-11-27 11:37:54 +0200

    [minor][runners] support inline custom rules (#41)
    * [minor] support inline custom rules

--- src/spec/usecase/passable.usecase.spec.js
+++ src/spec/usecase/passable.usecase.spec.js
@@ -4,6 +4,7 @@ import dev from '../../Passable';
 import usecase_a from './usecase_a';
 import usecase_b from './usecase_b';
 import usecase_c from './usecase_c';
+import usecase_d from './usecase_d';
 
 describe('Test Passable full usecase', () => {
 
@@ -26,6 +27,14 @@ describe('Test Passable full usecase', () => {
     it('should return correct response object for case: c', () => {
         const devData = usecase_c(dev),
             distData = usecase_c(dist);
+
+        expect(devData.response).to.deep.equal(devData.expect);
+        expect(distData.response).to.deep.equal(devData.expect);
+    });
+
+    it('should return correct response object for case: d', () => {
+        const devData = usecase_d(dev),
+            distData = usecase_d(dist);
 
         expect(devData.response).to.deep.equal(devData.expect);
         expect(distData.response).to.deep.equal(devData.expect);
