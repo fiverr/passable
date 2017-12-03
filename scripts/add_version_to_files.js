a44351e959c6e39f8d8788ddea3cabf17d3eebb3
Evyatar <evyatar.a@fiverr.com>
2017-12-03 12:27:21 +0200

    reduce instead of forEach

--- scripts/add_version_to_files.js
+++ scripts/add_version_to_files.js
@@ -5,21 +5,21 @@ const pattern = /{{([a-zA-Z0-9_]+)}}/;
 const strings = {
     'PASSABLE_VERSION': () => version
 };
-const matchedFiles = [];
 
-files.forEach((path) => {
+const matchedFiles = files.reduce((accumulator, path) => {
     if (path.toLowerCase().indexOf('.md') === -1) {
-        return;
+        return accumulator;
     }
 
     const content = fs.readFileSync(path, 'utf8');
     if (content.match(pattern)) {
-        matchedFiles.push({
+        accumulator.push({
             path,
             content
         });
     }
-});
+    return accumulator;
+}, []);
 
 matchedFiles.forEach(({ path, content }) => {
     fs.writeFileSync(path, content.replace(new RegExp(pattern, 'g'), (match) => {
