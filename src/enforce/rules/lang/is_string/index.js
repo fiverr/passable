21f8f9677550f9f0a568ec95f8920f137d66de7d
Evyatar Alush <evyatar.a@fiverr.com>
2018-01-26 14:15:02 +0200

    [Minor] Add default expect=true to rules (#60)

--- src/enforce/rules/lang/is_string/index.js
+++ src/enforce/rules/lang/is_string/index.js
@@ -1,7 +1,7 @@
 // @flow
 import { isType, expectType } from '../../helpers';
 
-function isString(value: mixed, expect: boolean): boolean {
+function isString(value: mixed, expect: boolean = true): boolean {
     expectType(expect, 'boolean', 'isString');
     return isType(value, 'string') === expect;
 }
