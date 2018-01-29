beffe1dcc135a5dc4b9afe1df5d514112eb7c9e0
Sahar Brodbeker <saharonb@gmail.com>
2018-01-29 17:36:31 +0200

    [PATCH] Build correct Sass assets path (#65)

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,7 +3,6 @@ cp ./README.md ./documentation/MAIN.md
 node ./scripts/make_version_file.js
 find ./documentation -type f > ./tree.txt
 node ./scripts/add_version_to_files.js
-npm i -g node-sass
 
 passable_version=$(<version.txt)
 
@@ -13,7 +12,7 @@ git config --global user.name $GIT_NAME
 git clone -b $DOCS_BRANCH https://github.com/$GITHUB_REPO.git docs
 rm -rf docs/*
 
-node-sass assets/style -o assets/style --output-style compressed
+npx node-sass documentation/assets/style -o documentation/assets/style --output-style compressed
 npx docpress b
 cd docs
 rm -rf assets/style/**/*.scss
