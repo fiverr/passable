9b5fc95cb15a4661d7cc04640b2bdc3ff2c658df
Evyatar Alush <evyatar.a@fiverr.com>
2018-03-03 02:56:07 +0200

    [Patch] Auto deploy next branch (#86)

--- scripts/make_version_file.js
+++ scripts/make_version_file.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const { version } = require('../package.json');
 
 fs.writeFileSync('version.json', JSON.stringify({ version }));
+fs.writeFileSync('./version.txt', version);
