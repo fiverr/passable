593739ca3e99fb89faae55e25239bfbf3f7d513a
Evyatar Alush <evyatar.a@fiverr.com>
2018-03-05 22:51:36 +0200

    [Minor] initialize proxy with all keys. Add support for proxy-polyfill. (#83)
    * [Minor] initialize proxy with all keys. Add support for proxy-polyfill.

--- src/Enforce/index.js
+++ src/Enforce/index.js
@@ -6,29 +6,31 @@ import * as runners from './runners';
 class Enforce {
     enforce: EnforceInstance;
     rules: EnforceRules;
+    allRunnables: EnforceRules;
 
     constructor(customRules: EnforceRules = {}) {
         this.rules = Object.assign({}, runnables.rules, customRules);
+        this.allRunnables = Object.assign({}, runnables.compounds, this.rules);
 
         return this.enforce;
     }
 
     enforce = (value: AnyValue) => {
-        const proxy: EnforceRules = new Proxy(this.rules, {
-            get: (rules, fnName) => {
+        const proxy: EnforceRules = new Proxy(this.allRunnables, {
+            get: (allRunnables, fnName) => {
 
-                if (rules.hasOwnProperty(fnName)) {
+                if (this.rules.hasOwnProperty(fnName) && typeof this.rules[fnName] === 'function') {
                     return (...args) => {
-                        runners.rule(rules[fnName], value, ...args);
+                        runners.rule(this.rules[fnName], value, ...args);
                         return proxy;
                     };
-                } else if (runnables.compounds.hasOwnProperty(fnName)) {
+                } else if (runnables.compounds.hasOwnProperty(fnName) && typeof runnables.compounds[fnName] === 'function') {
                     return (tests) => {
-                        runners.compound(rules, runnables.compounds[fnName], value, tests);
+                        runners.compound(this.rules, runnables.compounds[fnName], value, tests);
                         return proxy;
                     };
                 } else {
-                    return rules[fnName];
+                    return allRunnables[fnName];
                 }
             }
         });
