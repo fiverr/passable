dc8e8b8e11661d391f9f44b3aeb3a99c72a40567
Evyatar Alush <evyatar.a@fiverr.com>
2018-03-16 19:00:25 +0200

    [Patch] Use proxy polyfill when proxy is not supported (#95)

---
+++ src/Enforce/helpers/root.js
@@ -0,0 +1,10 @@
+let root;
+
+// credit https://stackoverflow.com/questions/3277182/how-to-get-the-global-object-in-javascript
+try {
+    root = (() => undefined).constructor('return this')();
+} catch (e) {
+    root = window;
+}
+
+export default root;
