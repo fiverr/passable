ab4f6a55ecfe97652eb1198c40c97acb50b90be5
Evyatar Alush <evyatar.a@fiverr.com>
2019-05-09 19:35:04 +0300

    [Major] change library name (#116)

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        library: 'Passable',
+        library: 'passable',
         libraryTarget: 'umd',
         libraryExport: 'default',
         globalObject: "Function('return this')()"
