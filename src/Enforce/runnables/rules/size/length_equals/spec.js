57d5dc56928a205a9444725dbfe50b79814358bb
Adi Feiner <adi.feiner@fiverr.com>
2019-05-26 16:41:38 +0300

    pr remarks

--- src/Enforce/runnables/rules/size/length_equals/spec.js
+++ src/Enforce/runnables/rules/size/length_equals/spec.js
@@ -1,43 +1,45 @@
-'use strict';
-
 import { expect } from 'chai';
-
+import faker from 'faker';
 import lengthEquals from './index';
 
 describe('Tests lengthEquals rule', () => {
+
+    const length = faker.random.number();
+    const word = faker.random.word();
+    const boolean = faker.random.boolean();
 
     describe('First argument is array or string', () => {
 
         describe('When first argument is equal to a given value', () => {
 
-            it('Should return true for an array equal to 5', () => {
-                expect(lengthEquals([1, 2, 3, 4, 5], 5)).to.equal(true);
+            it('Should return true for an array equal to length', () => {
+                expect(lengthEquals(new Array(length), length)).to.equal(true);
             });
 
-            it('Should return true for a string equal to 3', () => {
-                expect(lengthEquals('abc', 3)).to.equal(true);
+            it('Should return true for a string equal to word length', () => {
+                expect(lengthEquals(word, word.length)).to.equal(true);
             });
         });
 
         describe('When first argument is shorter', () => {
 
-            it('Should return false for an array shorter than 5', () => {
-                expect(lengthEquals([1, 2, 3, 4], 5)).to.equal(false);
+            it('Should return false for an array shorter than length', () => {
+                expect(lengthEquals(new Array(length), length + 1)).to.equal(false);
             });
 
-            it('Should return false for a string shorter than 3', () => {
-                expect(lengthEquals('ab', 3)).to.equal(false);
+            it('Should return false for a string shorter than word length', () => {
+                expect(lengthEquals(word, word.length + 1)).to.equal(false);
             });
         });
 
         describe('When first argument is longer', () => {
 
-            it('Should return false for an array longer than 5', () => {
-                expect(lengthEquals([1, 2, 3, 4, 5, 6], 5)).to.equal(false);
+            it('Should return false for an array longer than length', () => {
+                expect(lengthEquals(new Array(length), length - 1)).to.equal(false);
             });
 
-            it('Should return false for a string longer than 3', () => {
-                expect(lengthEquals('abcd', 3)).to.equal(false);
+            it('Should return false for a string longer than word length', () => {
+                expect(lengthEquals(word, word.length - 1)).to.equal(false);
             });
         });
     });
@@ -46,6 +48,14 @@ describe('Tests lengthEquals rule', () => {
 
         it('Should throw error', () => {
             expect(() => lengthEquals(undefined, 0)).to.throw(TypeError);
+        });
+
+        it('Should return false for number argument', () => {
+            expect(lengthEquals(length, 0)).to.equal(false);
+        });
+
+        it('Should return false for boolean argument', () => {
+            expect(lengthEquals(boolean, 0)).to.equal(false);
         });
     });
 
--- src/Enforce/runnables/rules/size/longer_than_or_equals/spec.js
+++ src/Enforce/runnables/rules/size/longer_than_or_equals/spec.js
@@ -1,50 +1,61 @@
-'use strict';
-
 import { expect } from 'chai';
+import faker from 'faker';
 import longerThanOrEquals from './index';
 
 describe('Tests longerThanOrEquals rule', () => {
+
+    const length = faker.random.number();
+    const word = faker.random.word();
+    const boolean = faker.random.boolean();
 
     describe('First argument is array or string', () => {
 
         describe('When first argument is longer', () => {
 
-            it('Should return true for an array longer than 5', () => {
-                expect(longerThanOrEquals([1, 2, 3, 4, 5, 6], 5)).to.equal(true);
+            it('Should return true for an array longer than length', () => {
+                expect(longerThanOrEquals(new Array(length), length - 1)).to.equal(true);
             });
 
-            it('Should return true for a string longer than 3', () => {
-                expect(longerThanOrEquals('abcd', 3)).to.equal(true);
+            it('Should return true for a string longer than word length', () => {
+                expect(longerThanOrEquals(word, word.length - 1)).to.equal(true);
             });
         });
 
         describe('When first argument is equal to a given value', () => {
 
-            it('Should return true for an array equal to 5', () => {
-                expect(longerThanOrEquals([1, 2, 3, 4, 5], 5)).to.equal(true);
+            it('Should return true for an array equal to length', () => {
+                expect(longerThanOrEquals(new Array(length), length)).to.equal(true);
             });
 
-            it('Should return true for a string equal to 3', () => {
-                expect(longerThanOrEquals('abc', 3)).to.equal(true);
+            it('Should return true for a string equal to word length', () => {
+                expect(longerThanOrEquals(word, word.length)).to.equal(true);
             });
         });
 
         describe('When first argument is shorter', () => {
 
-            it('Should return false for an array shorter than 5', () => {
-                expect(longerThanOrEquals([1, 2, 3, 4], 5)).to.equal(false);
+            it('Should return false for an array shorter than length', () => {
+                expect(longerThanOrEquals(new Array(length), length + 1)).to.equal(false);
             });
 
-            it('Should return false for a string shorter than 3', () => {
-                expect(longerThanOrEquals('ab', 3)).to.equal(false);
+            it('Should return false for a string shorter than word length', () => {
+                expect(longerThanOrEquals(word, word.length + 1)).to.equal(false);
             });
         });
     });
 
     describe('First argument isn\'t array or string', () => {
 
-        it('Should return undefined', () => {
+        it('Should throw error', () => {
             expect(() => longerThanOrEquals(undefined, 0)).to.throw(TypeError);
+        });
+
+        it('Should return false for number argument', () => {
+            expect(longerThanOrEquals(length, 0)).to.equal(false);
+        });
+
+        it('Should return false for boolean argument', () => {
+            expect(longerThanOrEquals(boolean, 0)).to.equal(false);
         });
     });
 });
