6c94b8970f0f763d923517d7860d8f9dda2d6b32
ealush <code@ealush.com>
2019-05-26 10:36:55 +0300

    revert test

--- src/spec/passable.api.specific.spec.js
+++ src/spec/passable.api.specific.spec.js
@@ -5,111 +5,111 @@ describe('Test running specific tests', () => {
     describe('Test array input', () => {
 
         it('Should only run first test', () => {
-            const output = specificTests(['First']);
+            const result = specificTests(['First']);
 
-            expect(output.skipped).to.deep.equal(['Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
-            expect(output.testCount).to.equal(1);
+            expect(result.skipped).to.deep.equal(['Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
+            expect(result.testCount).to.equal(1);
         });
 
         it('Should run Second, Third and Seventh Test', () => {
-            const output = specificTests(['Second', 'Third', 'Seventh']);
+            const result = specificTests(['Second', 'Third', 'Seventh']);
 
-            expect(output.skipped).to.deep.equal(['First', 'Fourth', 'Fifth', 'Sixth']);
-            expect(output.testCount).to.equal(3);
+            expect(result.skipped).to.deep.equal(['First', 'Fourth', 'Fifth', 'Sixth']);
+            expect(result.testCount).to.equal(3);
         });
 
         it('Should skip all tests', () => {
-            const output = specificTests(['IDoNotExist']);
+            const result = specificTests(['IDoNotExist']);
 
-            expect(output.skipped).to.deep.equal(['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
-            expect(output.testCount).to.equal(0);
+            expect(result.skipped).to.deep.equal(['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
+            expect(result.testCount).to.equal(0);
         });
 
         it('Should run all tests', () => {
-            const output = specificTests([]);
+            const result = specificTests([]);
 
-            expect(output.skipped).to.deep.equal([]);
-            expect(output.testCount).to.equal(8);
+            expect(result.skipped).to.deep.equal([]);
+            expect(result.testCount).to.equal(8);
         });
     });
 
     describe('Test string input', () => {
 
         it('Should only run first test', () => {
-            const output = specificTests('First');
+            const result = specificTests('First');
 
-            expect(output.skipped).to.deep.equal(['Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
-            expect(output.testCount).to.equal(1);
+            expect(result.skipped).to.deep.equal(['Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
+            expect(result.testCount).to.equal(1);
         });
 
         it('Should skip all tests', () => {
-            const output = specificTests('IDoNotExist');
+            const result = specificTests('IDoNotExist');
 
-            expect(output.skipped).to.deep.equal(['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
-            expect(output.testCount).to.equal(0);
+            expect(result.skipped).to.deep.equal(['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
+            expect(result.testCount).to.equal(0);
         });
 
         it('Should run all tests', () => {
-            const output = specificTests('');
+            const result = specificTests('');
 
-            expect(output.skipped).to.deep.equal([]);
-            expect(output.testCount).to.equal(8);
+            expect(result.skipped).to.deep.equal([]);
+            expect(result.testCount).to.equal(8);
         });
     });
 
     describe('Test object input', () => {
 
         it('Should only run first test', () => {
-            const output = specificTests({
+            const result = specificTests({
                 only: 'First'
             });
 
-            expect(output.skipped).to.deep.equal(['Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
-            expect(output.testCount).to.equal(1);
+            expect(result.skipped).to.deep.equal(['Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
+            expect(result.testCount).to.equal(1);
         });
 
         it('Should run Second, Third and Seventh Test', () => {
-            const output = specificTests({
+            const result = specificTests({
                 only: ['Second', 'Third', 'Seventh']
             });
 
-            expect(output.skipped).to.deep.equal(['First', 'Fourth', 'Fifth', 'Sixth']);
-            expect(output.testCount).to.equal(3);
+            expect(result.skipped).to.deep.equal(['First', 'Fourth', 'Fifth', 'Sixth']);
+            expect(result.testCount).to.equal(3);
         });
 
         it('Should skip all tests', () => {
-            const output = specificTests({
+            const result = specificTests({
                 only: 'IDoNotExist'
             });
 
-            expect(output.skipped).to.deep.equal(['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
-            expect(output.testCount).to.equal(0);
+            expect(result.skipped).to.deep.equal(['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']);
+            expect(result.testCount).to.equal(0);
         });
 
         it('Should skip Second and Fourth', () => {
-            const output = specificTests({
+            const result = specificTests({
                 not: ['Second', 'Fourth']
             });
 
-            expect(output.skipped).to.deep.equal(['Second', 'Fourth']);
-            expect(output.testCount).to.equal(6);
+            expect(result.skipped).to.deep.equal(['Second', 'Fourth']);
+            expect(result.testCount).to.equal(6);
         });
 
         it('Should skip First', () => {
-            const output = specificTests({
+            const result = specificTests({
                 not: 'First'
             });
 
-            expect(output.skipped).to.deep.equal(['First']);
-            expect(output.testCount).to.equal(7);
+            expect(result.skipped).to.deep.equal(['First']);
+            expect(result.testCount).to.equal(7);
         });
     });
 
     it('Should run all tests', () => {
-        const output = specificTests(null);
+        const result = specificTests(null);
 
-        expect(output.skipped).to.deep.equal([]);
-        expect(output.testCount).to.equal(8);
+        expect(result.skipped).to.deep.equal([]);
+        expect(result.testCount).to.equal(8);
     });
 });
 
