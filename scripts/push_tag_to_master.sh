9ece28b8ee0d47a3b87c0798152be6dbf23f38fa
ealush <code@ealush.com>
2019-06-28 01:39:31 +0300

    [Patch] Add verbose commit message

--- scripts/push_tag_to_master.sh
+++ scripts/push_tag_to_master.sh
@@ -2,6 +2,19 @@
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 
+echo "Cloning repo"
+git clone https://github.com/$TRAVIS_REPO_SLUG.git __temp
+cd __temp
+git checkout $TRAVIS_TAG
+
+echo "Getting list of changes"
+msg="$(git log --pretty=oneline --abbrev-commit --no-merges origin/master..)"
+echo "$msg"
+
+echo "Deleting cloned repo"
+cd ../
+rm -rf __temp
+
 echo "Removing old master"
 git branch -D master
 
@@ -14,9 +27,11 @@ npm version $TRAVIS_TAG --no-git-tag-version
 echo "Rebuilding with current tag"
 npm run build
 
+EMOJIS=(🚀 🤘 ✨ 🔔 🌈 🤯 ‼️)
+EMOJI=${EMOJIS[$RANDOM % ${#EMOJIS[@]}]}
+
 git add .
-git commit -m "Updating passable: $TRAVIS_TAG"
+git commit -m "$EMOJI Passable cumulative update: $TRAVIS_TAG" -m "$msg"
 
 echo "Pushing to master"
-
 git push https://${GITHUB_TOKEN}@github.com/$GITHUB_REPO.git master
