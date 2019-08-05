119fb4d66b9b9e8a6342435f37199591619be1fe
ealush <code@ealush.com>
2019-08-06 00:57:47 +0300

    [Config] Use correct version in next branches

--- scripts/prepare_next.sh
+++ scripts/prepare_next.sh
@@ -1,5 +1,5 @@
 echo "Preparing next tag"
-passable_version=$(<.version)
+passable_version=$(node -p -e "require('./package.json').version")
 next_version="${passable_version}-next-${TRAVIS_COMMIT:(0):6}"
 echo $next_version
 npm version $next_version --no-git-tag-version
