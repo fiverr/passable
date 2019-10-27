3994e30bcf79c3dff4d2a339259cf00fc3468343
Evyatar Alush <evyatar.a@fiverr.com>
2019-10-27 14:02:36 +0200

    Use anyone/any instead of any (#198)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import passable from './core/passable';
 import test from './core/test';
 import validate from './utilities/validate';
-import any from './utilities/any';
 import { WARN, FAIL } from './constants';
 import enforce from 'n4s/dist/enforce.min';
 
@@ -10,7 +9,7 @@ passable.enforce = enforce;
 passable.Enforce = enforce.Enforce;
 passable.test = test;
 passable.validate = validate;
-passable.any = any;
+passable.any = require('anyone/any');
 passable.WARN = WARN;
 passable.FAIL = FAIL;
 
