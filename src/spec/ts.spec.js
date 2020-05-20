7d41a31d761e5ccf429cf5f0627a48c46842aebc
Islam Attrash <isattrash@gmail.com>
2020-05-21 01:05:46 +0300

    fix

--- src/spec/ts.spec.js
+++ src/spec/ts.spec.js
@@ -1,8 +1,7 @@
 const { execSync } = require('child_process');
-const path = require('path');
 
 describe('TypeScript Typings', () => {
     it('Should pass the typings check', () => {
-        execSync(`node_modules/.bin/tsc ${path.resolve(__dirname, '../..', 'index.d.ts')}`);
+        execSync('node_modules/.bin/tsc index.d.ts');
     }).timeout(0);
 });
