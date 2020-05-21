399b1d6d80f22d719fa5cc7ad9178a1c268a3129
Islam Attrash <isattrash@gmail.com>
2020-05-21 10:45:35 +0300

    not happy path test

--- src/spec/ts.spec.js
+++ src/spec/ts.spec.js
@@ -1,7 +1,16 @@
+import { expect } from 'chai';
 const { execSync } = require('child_process');
 
 describe('TypeScript Typings', () => {
     it('Should pass the typings check', () => {
-        execSync('node_modules/.bin/tsc index.d.ts');
+        expect(() => {
+            execSync('node_modules/.bin/tsc index.d.ts');
+        }).to.not.throw();
+    }).timeout(0);
+
+    it('Should fail the typings check with fail stub', () => {
+        expect(() => {
+            execSync('node_modules/.bin/tsc src/spec/fail.d.ts');
+        }).to.throw();
     }).timeout(0);
 });
