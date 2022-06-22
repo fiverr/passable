b6bb9524eefc28fdc09b5b5f413e30a655a511e7
Islam Attrash <isattrash@gmail.com>
2022-06-22 23:34:12 +0300

    n4s@0.4.1

--- src/spec/react.elements.support.spec.js
+++ src/spec/react.elements.support.spec.js
@@ -1,17 +1,29 @@
-import passable, { enforce } from '../index';
+const runSpec = (passable) => {
+    describe('React Elements Support', () => {
+        let result;
 
-describe('React Elements Support', () => {
-    let result;
+        beforeEach(() => {
+            result = passable('NewUserForm', (test) => {
+                test('username', { a: 1 }, () => {
+                    passable.enforce(true).equals(false);
+                });
 
-    beforeEach(() => {
-        result = passable('NewUserForm', (test) => {
-            test('username', { a: 1 }, () => {
-                enforce(true).equals(false);
+                test('username', { a: 2 }, () => {
+                    enforce(true).equals(false);
+                }, passable.WARN);
             });
-        }).errors.username;
-    });
+        });
 
-    it('Default export should output correct version number (es6 imports)', () => {
-        expect(result).to.deep.equal([{ a: 1 }]);
+        it('should support objects in errors', () => {
+            expect(result.errors.username).to.deep.equal([{ a: 1 }]);
+        });
+
+        it('should support objects in warnings', () => {
+            expect(result.warnings.username).to.deep.equal([{ a: 2 }]);
+        });
     });
-});
+};
+
+runSpec(require('../'));
+runSpec(require('../../dist/passable'));
+runSpec(require('../../dist/passable.min.js'));
